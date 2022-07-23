5372f3da7b45729ac134abd3e82ca211db2ba3fe
nutsu7 <BivolLiviu62@gmail.com>
2022-07-24 01:59:58 +0300

    Added support to write to wmi directly
    Signed-off-by: nutsu7 <BivolLiviu62@gmail.com>

--- Utils/WMI.cs
+++ Utils/WMI.cs
@@ -7,12 +7,19 @@ using System.Threading.Tasks;
 
 namespace LegionControl.Utils
 {
-    internal class WMI
+	internal class WMI
 	{
-		public string GetData(string root, string instance, string arg)
-        {
+		internal string GetData(string root, string instance, string arg)
+		{
 			ManagementBaseObject mbo = new ManagementObject(root, instance, null).InvokeMethod(arg, null, null);
 			return mbo["Data"].ToString();
-        }
+		}
+		internal void SetData(string root, string instance, string param, string arg)
+        {
+			ManagementObject mo = new ManagementObject(root, instance, null);
+			ManagementBaseObject mp = mo.GetMethodParameters(param);
+			mp["Data"] = arg;
+			mo.InvokeMethod(param, mp, null);
+		}
 	}
 }
