184123f6956814e93922f9df75c1aa2bac545df8
nutsu7 <BivolLiviu62@gmail.com>
2022-07-23 15:40:19 +0300

    Added WMI communication method
    Signed-off-by: nutsu7 <BivolLiviu62@gmail.com>

---
+++ Utils/WMI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management;
+using System.Threading.Tasks;
+
+namespace LegionControl.Utils
+{
+    internal class WMI
+	{
+		public string GetData(string root, string instance, string arg)
+        {
+			ManagementBaseObject mbo = new ManagementObject(root, instance, null).InvokeMethod(arg, null, null);
+			return mbo["Data"].ToString();
+        }
+	}
+}
