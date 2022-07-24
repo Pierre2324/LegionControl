44874e5347211c4038ddca76ec86785e59b349aa
nutsu7 <BivolLiviu62@gmail.com>
2022-07-25 01:16:11 +0300

    Supported more variety of gathering wmi data
    Signed-off-by: nutsu7 <BivolLiviu62@gmail.com>

--- Utils/WMI.cs
+++ Utils/WMI.cs
@@ -9,10 +9,21 @@ namespace LegionControl.Utils
 {
 	internal class WMI
 	{
-		internal string GetData(string root, string instance, string arg)
+		private string output;
+		internal string GetDataGMZN(string root, string instance, string arg)
 		{
 			ManagementBaseObject mbo = new ManagementObject(root, instance, null).InvokeMethod(arg, null, null);
 			return mbo["Data"].ToString();
+		}
+
+		internal string GetDataSelect(string root, string arg, string item)
+        {
+			ManagementObjectSearcher mos = new ManagementObjectSearcher(root, arg);
+			foreach (ManagementObject mo in mos.Get())
+			{
+				output = mo[item].ToString();
+			}
+			return output;
 		}
 		internal void SetData(string root, string instance, string param, string arg)
         {
