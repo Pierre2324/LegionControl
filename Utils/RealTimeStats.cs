5ce1dcf8428f6b430935ea69938682f441d1b898
nutsu7 <BivolLiviu62@gmail.com>
2022-08-07 21:05:24 +0300

    Updated statuses

---
+++ Utils/RealTimeStats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using LegionControl.Utils;
+
+namespace LegionControl.Utils
+{
+    internal static class RealTimeStats
+    {
+
+        internal static string tempCurrentCPU;
+        internal static string tempCurrentGPU;
+        internal static string rpmCurrentCPU;
+        internal static string rpmCurrentGPU;
+
+        internal static void GetRTS()
+        {
+            Memory memory = new Memory();
+
+            tempCurrentCPU = memory.GetData("r", DeviceDetection.adrsTempCurrentCPU);
+            tempCurrentGPU = memory.GetData("r", DeviceDetection.adrsTempCurrentGPU);
+            rpmCurrentCPU = memory.GetData("r", DeviceDetection.adrsRpmCurrentCPU) + "00";
+            rpmCurrentGPU = memory.GetData("r", DeviceDetection.adrsRpmCurrentGPU) + "00";
+        }
+    }
+}
