8a7d4c0fb5c8ec0731bd377231cf5a2334565f30
Pierre-David Bélanger <belp0028@uqar.ca>
2022-08-16 20:23:18 -0400

    General: Remove unused imports

--- Utils/RealTimeStats.cs
+++ Utils/RealTimeStats.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using LegionControl.Utils;
-
-namespace LegionControl.Utils
+﻿namespace LegionControl.Utils
 {
     internal static class RealTimeStats
     {
