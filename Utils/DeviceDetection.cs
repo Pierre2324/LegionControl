a2732d4ea11a59ae8b138ab3d3a95651d5a9d90f
Pierre-David Bélanger <belp0028@uqar.ca>
2022-08-16 20:49:53 -0400

    DeviceDetection: Remove useless SetVars() [See commit message]
    This method is useless.
    First, it could be simplified to this since,
    AMD or not, addresses are the same. But there is also
    no reason to check if cpuName contains AMD to set the fan curve
    addresses just set them and check if AMD where we need it.
    So just initialize the variables normally. Those variables could as well be somewhere else where they're needed as they're useless here.
    
    adrsTempCurrentCPU = "0xFE00D5E6";
    adrsTempCurrentGPU = "0xFE00D5E7";
    adrsRpmCurrentCPU  = "0xFE00D4FE";
    adrsRpmCurrentGPU  = "0xFE00D406";
    
    if (cpuName.Contains("AMD"))
    {
        adrsFanCurveCPU    = new string[] { "0xFE00D540", "0xFE00D541", "0xFE00D542", "0xFE00D543", "0xFE00D544", "0xFE00D545", "0xFE00D546", "0xFE00D547", "0xFE00D548", "0xFE00D549", "0xFE00D54A" };
        adrsFanCurveGPU    = new string[] { "0xFE00D550", "0xFE00D551", "0xFE00D552", "0xFE00D553", "0xFE00D554", "0xFE00D555", "0xFE00D556", "0xFE00D557", "0xFE00D558", "0xFE00D559", "0xFE00D55A" };
    }

--- Utils/DeviceDetection.cs
+++ Utils/DeviceDetection.cs
@@ -7,12 +7,12 @@
 
         static string[] supportedModels = {"Legion 5 15IMH05H", "Legion 5 15ACH6H", "Legion 7 16ACHg6"};
 
-        internal static string   adrsTempCurrentCPU;
-        internal static string   adrsTempCurrentGPU;
-        internal static string   adrsRpmCurrentCPU;
-        internal static string   adrsRpmCurrentGPU;
-        internal static string[] adrsFanCurveCPU;
-        internal static string[] adrsFanCurveGPU;
+        internal static string   adrsTempCurrentCPU = "0xFE00D5E6";
+        internal static string   adrsTempCurrentGPU = "0xFE00D5E7";
+        internal static string   adrsRpmCurrentCPU  = "0xFE00D4FE";
+        internal static string   adrsRpmCurrentGPU  = "0xFE00D406";
+        internal static string[] adrsFanCurveCPU    = new string[] { "0xFE00D540", "0xFE00D541", "0xFE00D542", "0xFE00D543", "0xFE00D544", "0xFE00D545", "0xFE00D546", "0xFE00D547", "0xFE00D548", "0xFE00D549", "0xFE00D54A" };
+        internal static string[] adrsFanCurveGPU    = new string[] { "0xFE00D550", "0xFE00D551", "0xFE00D552", "0xFE00D553", "0xFE00D554", "0xFE00D555", "0xFE00D556", "0xFE00D557", "0xFE00D558", "0xFE00D559", "0xFE00D55A" };
 
         internal static void Initialize()
         {
@@ -21,9 +21,7 @@
             model = wmi.GetDataSelect("root\\CIMV2", "SELECT * FROM Win32_ComputerSystemProduct", "Version");
             cpuName = wmi.GetDataSelect("root\\CIMV2", "SELECT * FROM Win32_Processor", "Name");
 
-            if (Verify())
-                SetVars();
-            else
+            if (!Verify())
             {
                 string invalidDeviceMsg = "Current device is incompatible with the Legion Control software.\n" +
                                           "Make sure you have one of the following models :\n";
@@ -39,32 +37,10 @@
         private static bool Verify()
         {
             foreach (var sm in supportedModels)
-            {
                 if (model == sm)
                     return true;
-            }   
+
             return false;
         }
-
-        private static void SetVars()
-        {
-            if (cpuName.Contains("AMD"))
-            {
-                adrsTempCurrentCPU = "0xFE00D5E6";
-                adrsTempCurrentGPU = "0xFE00D5E7";
-                adrsRpmCurrentCPU  = "0xFE00D4FE";
-                adrsRpmCurrentGPU  = "0xFE00D406";
-                adrsFanCurveCPU    = new string[] { "0xFE00D540", "0xFE00D541", "0xFE00D542", "0xFE00D543", "0xFE00D544", "0xFE00D545", "0xFE00D546", "0xFE00D547", "0xFE00D548", "0xFE00D549", "0xFE00D54A" };
-                adrsFanCurveGPU    = new string[] { "0xFE00D550", "0xFE00D551", "0xFE00D552", "0xFE00D553", "0xFE00D554", "0xFE00D555", "0xFE00D556", "0xFE00D557", "0xFE00D558", "0xFE00D559", "0xFE00D55A" };
-            }
-            else
-            {
-                adrsTempCurrentCPU = "0xFF00D5E6";
-                adrsTempCurrentGPU = "0xFF00D5E7";
-                adrsRpmCurrentCPU  = "0xFF00D4FE";
-                adrsRpmCurrentGPU  = "0xFF00D406";
-            }
-        }
-
     }
 }
